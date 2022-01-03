305325887ad3c8d671c44c8ce5ea50ebeb031636
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-03 07:24:41 +0530

    Added - Vending Machine Program

--- src/com/example/junitprogram/JUnitProgram.java
+++ src/com/example/junitprogram/JUnitProgram.java
@@ -1,7 +1,31 @@
 package com.example.junitprogram;
 
+import java.util.Scanner;
+
 public class JUnitProgram {
     public static void main(String[] args) {
-        System.out.println("Welcome to JUnit program");
+        vendingMachine();
+    }
+    static  void vendingMachine(){
+        //Declaring notes in array
+        int [] change = {1000, 500, 100, 50, 10, 5, 2, 1};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the amount of which change you want: ");
+        int number = scanner.nextInt();
+        int totalNotes = 0, noOfNotes;
+        if(number > 0){
+            for(int p = 0; p < change.length; p++){
+
+                if (change[p] <= number) {
+                    noOfNotes = number / change[p];
+                    totalNotes = totalNotes + noOfNotes;
+                    System.out.println("Notes required of " + change[p] + " = " + noOfNotes );
+                    number = number % change[p];
+                }
+            }
+            System.out.println("Minimum Notes required : " + totalNotes);
+        }else{
+            System.out.println("Change cannot be negative!");
+        }
     }
 }

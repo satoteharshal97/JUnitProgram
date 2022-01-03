22844a883a2eb26b9e95987462d0309affa479d5
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-03 07:29:31 +0530

    Added - Temperature Conversion program

--- src/com/example/junitprogram/JUnitProgram.java
+++ src/com/example/junitprogram/JUnitProgram.java
@@ -4,22 +4,13 @@ import java.util.Scanner;
 
 public class JUnitProgram {
     public static void main(String[] args) {
-        dayOfWeek();
+        temperatureConversion();
     }
-    static void dayOfWeek(){
-        int m0, y0, d0, x;
+    static void temperatureConversion(){
+        System.out.println("Enter the temperature in celsius: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the month:");
-        int month = scanner.nextInt();
-        System.out.println("Enter the year:");
-        int year = scanner.nextInt();
-        System.out.println("Enter the day:");
-        int day = scanner.nextInt();
-        String [] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        y0 = year - (14 - month) / 12;
-        x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
-        m0 = month + 12 * ((14 - month) / 12) - 2;
-        d0 = (day + x + (31 * m0) / 12) % 7;
-        System.out.println(days[d0]);
+        double Celcius = scanner.nextDouble();
+        double Fahrenheit = (Celcius * (9 / 5)) + 32;
+        System.out.println("Temperature in Fahrenheit: " + Fahrenheit);
     }
 }

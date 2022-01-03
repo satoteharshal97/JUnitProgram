f10400d98af1d5126091b1c24f1102f9fe146db1
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-03 07:21:47 +0530

    Added - Welcome to JUnit Programs

--- src/com/example/junitprogram/JUnitProgram.java
+++ src/com/example/junitprogram/JUnitProgram.java
@@ -1,5 +1,7 @@
 package com.example.junitprogram;
 
 public class JUnitProgram {
-    
+    public static void main(String[] args) {
+        System.out.println("Welcome to JUnit program");
+    }
 }

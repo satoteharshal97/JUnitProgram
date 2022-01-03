d8d9e698e37f5643591c3f41b63e7300fc33032b
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-03 07:31:32 +0530

    Added - Monthly payment program

--- src/com/example/junitprogram/JUnitProgram.java
+++ src/com/example/junitprogram/JUnitProgram.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class JUnitProgram {
     public static void main(String[] args) {
-        temperatureConversion();
+        monthlyPayment();
     }
-    static void temperatureConversion(){
-        System.out.println("Enter the temperature in celsius: ");
+    static void monthlyPayment(){
         Scanner scanner = new Scanner(System.in);
-        double Celcius = scanner.nextDouble();
-        double Fahrenheit = (Celcius * (9 / 5)) + 32;
-        System.out.println("Temperature in Fahrenheit: " + Fahrenheit);
+        System.out.println("Enter Years: ");
+        double years = scanner.nextDouble();
+        System.out.println("Enter Principal amount: ");
+        double principal = scanner.nextDouble();
+        System.out.println("Enter Rate: ");
+        double rate = scanner.nextDouble();
+        double n = 12 * years;
+        double r = rate / (12 * 100);
+        double payment = (principal * r ) / (1 -Math.pow((1 + r), (-n)));
+        System.out.println("Monthly Payment : " + payment);
     }
 }
